79719fb0fe8f80c33b52eff58fe59c112746afc9
Aashish <84653689+ashish71-ui@users.noreply.github.com>
2024-02-06 13:13:21 +0545

    Update settings.py
    settting

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@30ro$ljg26ebsuoo)st+ou*%&%tu2mm^cv*=g_r)-(+)3isl$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["administration.pythonanywhere.com"]
 
 
 # Application definition
